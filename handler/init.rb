83de4625bc967dd4cd787131489e48d9727092b3
Scott Gerike <scott.gerike@kineticdata.com>
2015-09-28 10:35:01 -0500

    More verbose error logging

--- handler/init.rb
+++ handler/init.rb
@@ -1,7 +1,9 @@
 # Require the dependencies file to load the vendor libraries
 require File.expand_path(File.join(File.dirname(__FILE__), 'dependencies'))
+# Require the Office 365 Authentication file
+require File.expand_path(File.join(File.dirname(__FILE__), 'o365_authentication'))
 
-class MsprojectProjectTaskCreateV1
+class MsprojectProjectTaskCreateV2
   def initialize(input)
     # Set the input document attribute
     @input_document = REXML::Document.new(input)
@@ -21,13 +23,8 @@ class MsprojectProjectTaskCreateV1
   end
 
   def execute()
-    resources_path = File.join(File.expand_path(File.dirname(__FILE__)), 'resources')
-
-    # Create the command string that will be used to retrieve the cookies
-    cmd_string = "O365Auth.Console.exe #{@info_values['ms_project_location']} #{@info_values['username']} #{@info_values['password']} #{@info_values['integrated_authentication']}"
-
     # Retrieve the cookies
-    cookies = `cd "#{resources_path}" & #{cmd_string}`
+    cookies = get_office365_cookies(@info_values['ms_project_location'],@info_values['username'],@info_values['password'])
 
     proj_resource = RestClient::Resource.new(@info_values['ms_project_location'],
     :headers => {:content_type => "application/json",:accept => "application/json", :cookie => cookies})
@@ -43,11 +40,11 @@ class MsprojectProjectTaskCreateV1
 
     task_endpoint = proj_resource["/_api/ProjectServer/Projects('#{project_id}')/Draft/Tasks"]
 
-    puts "Creating a Task for the Project '#{project_id}" if @enable_debug_logging
+    puts "Creating a Task for the Project '#{project_id}'" if @enable_debug_logging
     begin
       results = task_endpoint.post update_params.to_json
     rescue RestClient::Exception => error
-      raise StandardError, error.inspect
+      handle_error(error)
     end
 
     puts "Parsing the result to get the task id" if @enable_debug_logging
@@ -55,7 +52,7 @@ class MsprojectProjectTaskCreateV1
 
     puts json.inspect
     # Get the JSON value array that contains the lookup table information
-    task_id = json["id"]
+    task_id = json["Id"]
 
     puts "Returning results" if @enable_debug_logging
     <<-RESULTS
@@ -65,9 +62,43 @@ class MsprojectProjectTaskCreateV1
     RESULTS
   end
 
+  def handle_error(error)
+    error_message = error.inspect
+
+    code = nil
+    value = nil
+    begin
+      json = JSON.parse(error.response.to_s)
+      if !json["odata.error"].nil?
+        if !json["odata.error"]["message"].nil? && !json["odata.error"]["message"]["value"].nil?
+          error_message = json["odata.error"]["message"]["value"].to_s
+          value = json["odata.error"]["message"]["value"]
+        end
+
+        if !json["odata.error"]["code"].nil?
+          if json["odata.error"]["code"].split(",").length > 1
+            if json["odata.error"]["code"].split(",")[1].strip == "Microsoft.SharePoint.Client.ResourceNotFoundException"
+              error_message = "Invalid Project: Can't find a project with an id of '#{@parameters['project_id']}'"
+            else
+              code = json["odata.error"]["code"].split(",")[0].strip
+            end
+          end
+        end
+      end
+    rescue Exception
+      # If the Response data can't be parsed, throw a standard error
+      raise StandardError, error.inspect
+    end
+
+    if code != nil && value != nil
+      error_message = "Error Name: #{value}, Code: #{code}. Too see more details about this error, see Project Server 2013 error codes (https://msdn.microsoft.com/en-us/library/office/ms508961.aspx)."
+    end
+    raise StandardError, error_message
+  end
+
   def set_form_digest(proj_resource)
     context_endpoint = proj_resource["/_api/contextinfo"]
-    puts "Sending a request to get the FormDigestValue that will be passed at the X-RequestDigest header in the create call"
+    puts "Sending a request to get the FormDigestValue that will be passed at the X-RequestDigest header in the create call" if @enable_debug_logging == true
     begin
       results = context_endpoint.post ""
     rescue RestClient::Exception => error
@@ -77,9 +108,9 @@ class MsprojectProjectTaskCreateV1
     json = JSON.parse(results)
     # Get the JSON value array that contains the lookup table information
     form_digest_value = json["FormDigestValue"]
-    puts "Retrieved Form Digest: #{form_digest_value}"
+    puts "Retrieved Form Digest: #{form_digest_value}" if @enable_debug_logging
     proj_resource.headers["X-RequestDigest"] = form_digest_value
-end
+  end
 
   # This is a template method that is used to escape results values (returned in
   # execute) that would cause the XML to be invalid.  This method is not
--- test/simple_input.rb
+++ test/simple_input.rb
@@ -2,13 +2,12 @@
   'info' => {
     'username' => '',
     'password' => '',
-    'integrated_authentication' => 'false',
     'ms_project_location' => '',
     'enable_debug_logging' => 'Yes'
   },
   'parameters' => {
     'project_id' => '',
-    'name' => 'test name',
-    'notes' => 'test note'
+    'name' => 'Testing Task',
+    'notes' => 'Note'
   }
 }
